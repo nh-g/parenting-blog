1182862f7ebd2984bfae9378ca4f0bc507ae84d1
nh-g <giangngohong@gmail.com>
2021-04-15 20:52:48 +0200

    created page Home and render onto App using Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import Navigation from './components/Navigation';
+import Home from './pages/Home'
+
 import './styles/base/style.scss';
 function App() {
   return (
     <Router>
       <div className="App">
         <Navigation />
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route component ={() => 404}/>
+        </Switch>
       </div>
     </Router>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+export default function Home(){
+    return(
+        <div>This is HOME</div>
+    )
+}
