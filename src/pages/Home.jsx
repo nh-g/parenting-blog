d6ff36a9d9fda7eb8b2c8420d066e122b51c1c13
nh-g <giangngohong@gmail.com>
2021-04-23 23:19:29 +0200

    fetch featured post, passing props to Post components, added feature posts config

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 import trending from '../assets/data/trending'
+import featured from '../assets/data/feature.jsx'
+
 import PostMasonry from '../components/common/PostMasonry'
+import MasonryPost from '../components/common/MasonryPost'
 
 const trendingConfig = {
     1: {
@@ -10,22 +13,42 @@ const trendingConfig = {
     }
 }
 
+const featureConfig = {
+    0: {
+        gridArea: '1 / 1 / span 1 / span 2',
+        height: '300px'
+    },
+    1: {
+        height: '300'
+    },
+    3: {
+        height: '630px',
+        marginLef: '30px',
+        width: '630px'
+    }
+}
+
 const mergeStyles = (posts, config) => {
     posts.forEach((post, index) => {
         post.style = config[index]
     })
 }
-
 mergeStyles(trending, trendingConfig)
+mergeStyles(featured, featureConfig)
 
+const lastFeatured = featured.pop()
 export default function Home() {
     return (
-        <section className ="container home">
-            <div className ="row">
-                <h2>Trending Posts</h2>
-                <PostMasonry posts ={trending} columns={3}/>
-            </div>
-
-        </section>
-    )
+      <section className="container home">
+        <div className="row">
+          <section className="featured-posts-container">
+            <h1>Feature Posts</h1>
+            <PostMasonry  posts={featured} columns={2} tagsOnTop={true}/>
+            <MasonryPost post = {lastFeatured} tagsOnTop={true}/>
+          </section>
+          <h1>Trending Posts</h1>
+          <PostMasonry posts={trending} columns={3} />
+        </div>
+      </section>
+    );
 }
