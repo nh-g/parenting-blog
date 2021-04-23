4aa40b2a92ae5d4fdb0d5152096fd2af203aaceb
nh-g <giangngohong@gmail.com>
2021-04-23 22:33:24 +0200

    passing background color of category to Masonry Post

--- src/components/common/MasonryPost.jsx
+++ src/components/common/MasonryPost.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
+import {categoryColors} from './categoryColors'
 
 export default function MasonryPost({ post, tagsOnTop }) {
     const background_style = {
       backgroundImage: `url("${require(`../../assets/images/${post.image}`).default}")`};
   return (
-      <a className = 'masonry-post overlay' style = {background_style} href={post.link}>
-          <div className ="image-text">
-              <div>
-                  <h2 className="image-title">
-                      {post.title}
-                  </h2>
-                  <span className="image-date">{post.date}</span>
-              </div>
-          </div>
-      </a>
+    <a
+      className="masonry-post overlay"
+      style={background_style}
+      href={post.link}
+    >
+      <div className="image-text">
+        <div className="tags-container">
+          {post.categories.map((tag, index) =>
+          <span key = {index} className="tag" style={{backgroundColor: categoryColors[tag]}}>
+              {tag.toUpperCase()}
+          </span>
+          )}
+          <h2 className="image-title">{post.title}</h2>
+          <span className="image-date">{post.date}</span>
+        </div>
+      </div>
+    </a>
   );
 }
