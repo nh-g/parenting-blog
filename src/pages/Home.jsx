21e6e078676ef58cdc68126f31cfc39eff2b6483
nh-g <giangngohong@gmail.com>
2021-04-23 22:34:34 +0200

    created config for trending posts and merged it with styles of post passed from PostMasonry

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 
 import trending from '../assets/data/trending'
 import PostMasonry from '../components/common/PostMasonry'
+
+const trendingConfig = {
+    1: {
+        gridArea: '1 / 2/ span 2/ span 1'
+                // gridArea: '1 / 2/ 3/ 3'
+    }
+}
+
+const mergeStyles = (posts, config) => {
+    posts.forEach((post, index) => {
+        post.style = config[index]
+    })
+}
+
+mergeStyles(trending, trendingConfig)
+
 export default function Home() {
     return (
         <section className ="container home">
