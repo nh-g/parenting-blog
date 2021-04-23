04874ff7e6bfd31fd55ceb1477ed35ef2eddb222
nh-g <giangngohong@gmail.com>
2021-04-23 06:44:24 +0200

    passing props's data taken from mock data to PostMasonry

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,15 @@
-export default function Home(){
-    return(
-        <main className = "home"> This is HOME</main>
+import React from 'react'
+
+import trending from '../assets/data/trending'
+import PostMasonry from '../components/common/PostMasonry'
+export default function Home() {
+    return (
+        <section className ="container home">
+            <div className ="row">
+                <h2>Trending Posts</h2>
+                <PostMasonry posts ={trending} columns={3}/>
+            </div>
+
+        </section>
     )
 }
