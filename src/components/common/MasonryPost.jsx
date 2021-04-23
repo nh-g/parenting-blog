b20faa39e5da1a8a8bccc012b8ba3446d1959075
nh-g <giangngohong@gmail.com>
2021-04-24 00:11:54 +0200

    added style of text inside the post to the bottom of container

--- src/components/common/MasonryPost.jsx
+++ src/components/common/MasonryPost.jsx
@@ -9,22 +9,26 @@ export default function MasonryPost({ post, tagsOnTop }) {
     const style = windowWidth > 900 ? { ... background_image, ...post.style}: { ...background_image}
   
       return (
-    <a
-      className="masonry-post overlay"
-      style={style}
-      href={post.link}
-    >
-      <div className="image-text">
-        <div className="tags-container">
-          {post.categories.map((tag, index) =>
-          <span key = {index} className="tag" style={{backgroundColor: categoryColors[tag]}}>
-              {tag.toUpperCase()}
-          </span>
-          )}
-          <h2 className="image-title">{post.title}</h2>
-          <span className="image-date">{post.date}</span>
-        </div>
-      </div>
-    </a>
-  );
+        <a className="masonry-post overlay" style={style} href={post.link}>
+          <div
+            className="image-text"
+            style={{ justifyContent: "flex-end" }}
+            // style={{ justifyContent: tagsOnTop ? "space-between" : "flex-end" }}
+          >
+            <div className="tags-container">
+              {post.categories.map((tag, index) => (
+                <span
+                  key={index}
+                  className="tag"
+                  style={{ backgroundColor: categoryColors[tag] }}
+                >
+                  {tag.toUpperCase()}
+                </span>
+              ))}
+              <h2 className="image-title">{post.title}</h2>
+              <span className="image-date">{post.date}</span>
+            </div>
+          </div>
+        </a>
+      );
 }
