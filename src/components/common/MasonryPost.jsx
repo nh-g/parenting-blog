86155a2196a56af203c25d2f67f036b2a5dad289
nh-g <giangngohong@gmail.com>
2021-04-23 14:08:24 +0200

    background image works by adding '.default' after 'require'

--- src/components/common/MasonryPost.jsx
+++ src/components/common/MasonryPost.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
 export default function MasonryPost({ post, tagsOnTop }) {
-    const style = {backgroundImage: `url("${require(`../../assets/images/${post.image}`)}")`}
+    const background_style = {
+      backgroundImage: `url("${require(`../../assets/images/${post.image}`).default}")`};
   return (
-      <a className = 'masonry-post overlay' style = {style} href={post.link}>
-          <div className ="image-text"></div>
+      <a className = 'masonry-post overlay' style = {background_style} href={post.link}>
+          <div className ="image-text">
+              <div>
+                  <h2 className="image-title">
+                      {post.title}
+                  </h2>
+                  <span className="image-date">{post.date}</span>
+              </div>
+          </div>
       </a>
   );
 }
