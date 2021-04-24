cd53a68e08e528abcb2550ba5555489981f08200
nh-g <giangngohong@gmail.com>
2021-04-24 09:53:00 +0200

    refactor home container to posts' sections; added PostGrid component section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 import trending from '../assets/data/trending'
 import featured from '../assets/data/feature.jsx'
 
-import PostMasonry from '../components/common/PostMasonry'
-import MasonryPost from '../components/common/MasonryPost'
+import {PostMasonry, MasonryPost, PostGrid} from '../components/common'
 
 const trendingConfig = {
     1: {
@@ -31,24 +30,42 @@ const featureConfig = {
 const mergeStyles = (posts, config) => {
     posts.forEach((post, index) => {
         post.style = config[index]
+        post.author = 'N.H.G'
+        post.description =
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas venenatis erat a purus ullamcorper, rhoncus sodales ligula auctor. Fusce convallis metus ut dolor ultrices, ut accumsan quam consectetur. Quisque at eleifend arcu. Fusce finibus imperdiet elit, vitae bibendum eros lobortis nec. Sed a libero porttitor, bibendum mi ac, vestibulum ipsum. Vestibulum non tincidunt nibh. Morbi posuere leo nisl, non aliquam erat finibus vitae. Maecenas pretium, mauris vel ultricies vestibulum, augue metus ultricies mi, a fermentum erat nisl accumsan nibh. Vivamus at rutrum mi. Etiam sem orci, bibendum ac tristique quis, auctor ut nunc. Integer et pharetra nisl. Aenean a finibus sapien. Nullam felis arcu, pulvinar eu interdum in, cursus ac lacus."
     })
 }
 mergeStyles(trending, trendingConfig)
 mergeStyles(featured, featureConfig)
 
 const lastFeatured = featured.pop()
+const recentPosts = {...trending, ...featured, ...featured}
 export default function Home() {
     return (
-      <section className="container home">
-        <div className="row">
-          <h1>Featured Posts</h1>
-          <section className="featured-posts-container">
-            <PostMasonry posts={featured} columns={2} tagsOnTop={true} />
-            <MasonryPost post={lastFeatured} tagsOnTop={true} />
-          </section>
-          <h1>Trending Posts</h1>
-          <PostMasonry posts={trending} columns={3} />
-        </div>
-      </section>
+      <main className="home">
+        <section className="container">
+          <div className="row">
+            <h1>Featured Posts</h1>
+            <section className="featured-posts-container">
+              <PostMasonry posts={featured} columns={2} tagsOnTop={true} />
+              <MasonryPost post={lastFeatured} tagsOnTop={true} />
+            </section>
+          </div>
+        </section>
+
+        <section className="container">
+          <div className="row">
+            <h1>Recent Posts</h1>
+            <PostGrid posts = {recentPosts}/>
+          </div>
+        </section>
+
+        <section className="container">
+          <div className="row">
+            <h1>Trending Posts</h1>
+            <PostMasonry posts={trending} columns={3} />
+          </div>
+        </section>
+      </main>
     );
 }
